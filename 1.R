8f853d05797190d5ccdff0758b9a1ace0ece883e
testcourse <jguo2@jguo2-WS.cisco.com>
2014-05-10 18:26:46 +0800

    1

--- 1.R
+++ 1.R
@@ -5,7 +5,7 @@ SCC <- readRDS("Source_Classification_Code.rds");
 
 data <- tapply(NEI$Emissions,NEI$year,sum);
 
-plot(as.numeric(names(data)),as.numeric(data), type='l', xlab="Year", ylab="Total Emissions");
+plot(as.numeric(names(data)),as.numeric(data), type='l', xlab="Year", ylab="Total Emissions (tons)");
 
 dev.copy(png,"1.png",width=480, height=480);
 
