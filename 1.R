f28b0bde9e3c129d467ad4f80222ef49c2452573
testcourse <jguo2@jguo2-WS.cisco.com>
2014-05-10 18:15:26 +0800

    1

---
+++ 1.R
@@ -0,0 +1,12 @@
+
+NEI <- readRDS("summarySCC_PM25.rds");
+
+SCC <- readRDS("Source_Classification_Code.rds");
+
+data <- tapply(NEI$Emissions,NEI$year,sum);
+
+plot(as.numeric(names(data)),as.numeric(data), type='l', xlab="Year", ylab="Total Emissions");
+
+dev.copy(png,"1.png",width=480, height=480);
+
+dev.off();
